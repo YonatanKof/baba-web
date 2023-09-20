1781cdab620543234a84b23b7fc274644843e299
Yonatan Kof <kof@yonatankof.com>
2023-09-20 21:56:20 +0300

    Init content

--- app.vue
+++ app.vue
@@ -1,5 +1,32 @@
+<script setup>
+const { data } = await useAsyncData('equal', () => {
+	return queryContent('services').where({}).find();
+});
+</script>
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<main class="u-container">
+		<h1>Oded Babayoff</h1>
+		<h2>Design Consulting Services</h2>
+		<p>
+			With over 15 years of extensive experience in leading design teams across diverse tech organizations, I
+			successfully delivered numerous products to market and developed winning design strategies.
+		</p>
+		<p>
+			I have established many design organizations from scratch and integrated UX best practices and agile methodologies
+			within product teams, resulting in substantial enhancements in velocity and efficiency.
+		</p>
+		<p>
+			Over the last decade I gained the experience in creating strong design organizations that align with business
+			priorities & goals.
+		</p>
+
+		<h2>Services</h2>
+		<ItemList :item-list="data" />
+	</main>
 </template>
+<style lang="scss" scoped>
+// main {
+// 	max-width: 1280px;
+// 	width: 75;
+// }
+</style>
---
+++ components/ItemList.vue
@@ -0,0 +1,61 @@
+<script setup>
+const props = defineProps({
+	itemList: {
+		type: Array,
+		required: true,
+	},
+	message: {
+		type: String,
+		default: 'There are no designs right now, but stay tuned for newer releases in the future.',
+	},
+});
+// const { $formatDate } = useNuxtApp();
+</script>
+
+<template>
+	<main class="u-grid grid">
+		<div v-for="service in itemList" :key="service._path">
+			<NuxtLink :to="service._path + '/'">
+				<img :src="service.image" alt="" />
+				<h3>{{ service.title }}</h3>
+				<p>{{ service.description }}</p>
+			</NuxtLink>
+		</div>
+	</main>
+	<p v-if="itemList.length == 0">
+		{{ message }}
+	</p>
+</template>
+
+<style scoped>
+a{
+	text-decoration: none;
+}
+main {
+	gap: var(--space-s);
+}
+.grid {
+	/* display: grid; */
+	grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
+}
+
+div {
+	margin-block-end: var(--space-s);
+	background-color: aliceblue;
+	box-shadow: var(--shadow-md);
+	overflow: auto;
+	transition: all ease-in-out 0.25s;
+	&:hover {
+		box-shadow: var(--shadow-lg);
+		transform: scale(1.01);
+	}
+}
+a {
+	padding: var(--space-s);
+	inset: 0;
+	display: block;
+}
+p{
+	font-size: var(--step--1);
+}
+</style>
