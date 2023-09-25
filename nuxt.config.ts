da1b6d5f2fdf9f463d04689296873135e7880a51
Yonatan Kof <kof@yonatankof.com>
2023-09-25 16:22:40 +0300

    Add pageTransition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,12 @@ export default defineNuxtConfig({
 			},
 		},
 	},
+	app: {
+		pageTransition: {
+			name: 'page',
+			mode: 'out-in', // default
+		},
+	},
 	vite: {
 		css: {
 			preprocessorOptions: {
