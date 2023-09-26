b10ce4ffbfbfef0e1d7f8f6d67aee55d0911a902
Yonatan Kof <kof@yonatankof.com>
2023-09-26 09:48:36 +0300

    Some info in the contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,9 +1,20 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Contact: TBD!
-  </div>
+	<main>
+    <hr>
+		<h3>Contact me</h3>
+    <a href="mailto:babayof.oded@gmail.com">Send me an email</a> 
+    <br>
+    <a href="tel:+972544306559">Or give me a call</a>
+		<h3>Find me online</h3>
+    <nuxt-link to="https://www.linkedin.com/in/odedbb/">Linkedin</nuxt-link> • 
+    <nuxt-link to="https://adplist.org/mentors/oded-babayoff">ADPList</nuxt-link>
+	</main>
 </template>
 
-<style scoped></style>
+<style scoped>
+h3{
+  margin-top: var(--space-l);
+  margin-bottom: var(--space-3xs);
+}</style>
